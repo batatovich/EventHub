5bb963e4dc719dfaa0e88c5b4d607fc9658ca5c2
Peter <pllaurado@membranelabs.com>
2024-08-22 11:11:34 -0300

    Header component

--- client/src/app/(auth)/layout.js
+++ client/src/app/(auth)/layout.js
@@ -1,13 +1,10 @@
-import '.././globals.css';
+import Header from '@/components/Header';
+import '../globals.css';
 
 export default function AuthLayout({ children }) {
   return (
     <div>
-      <header className="w-full h-20 bg-transparent border-b border-gray-300 sticky top-0 z-50 ">
-        <div className="container mx-auto px-3 h-full flex items-center">
-          <h1 className="text-2xl font-bold text-black">EventHub</h1>
-        </div>
-      </header>
+      <Header title="EventHub" />
       <main>{children}</main>
     </div>
   );
